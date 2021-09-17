9b2418b8915b9ac177b2c7dc3c33a9467873e1f0
brunodovalcallao <24bdovalcallao@athenian.org>
2021-09-17 01:02:29 -0700

    i had trouble with this one, because i was falling asleep during class and could barely pay attention. can we work on it during class? I've already spent a long time on it.

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,28 +4,44 @@ public class MyMain {
     // If the char is not an uppercase letter, 
     // it is returned unchanged. 
     public static char toLower(char ch) {
-        // REPLACE WITH YOUR CODE
-        return '!';
+        //this is a capital letter
+        if (ch >= 65 && ch <=90) return (char) ((char) ch + 32);
+        return (char) (ch+32);
+
+
     }
 
     // Changes a lowercase letter to an uppercase letter
     // If the char is not a lowercase letter,
     // it is returned unchanged.
     public static char toUpper(char ch) {
-        // REPLACE WITH YOUR CODE HERE
-        return '!';
+        if (ch >= 65 && ch <=90){
+            return ch;
+
+        }
+
+        return (char) (ch - 32);
     }
 
     // The method is given a String as input and returns a
     // new String where all of the numbers have been removed
     // from the original String.
     public static String removeNumbers(String str) {
-        // REPLACE WITH YOUR CODE
-        return "!!!";
-    }
+       //between 48 and 57
+        String total = "";
+        for (int i = 0; i<= str.length();i++){
+            char ch = (char) i;
+            if (ch <48 || ch >57){
+                total += str.charAt(i);
+            }
+            return total;
+
+        }
+
+
 
     // Write some code to test your methods!
     public static void main(String[] args) {
         // ADD SOME TESTING CODE HERE
-    }
+        {
 }
