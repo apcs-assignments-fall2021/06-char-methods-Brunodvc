a1c04bba368e2ab19df247b7a71cda5c43216668
brunodovalcallao <24bdovalcallao@athenian.org>
2021-09-18 14:31:23 -0700

    i finally got it to work

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,43 +5,48 @@ public class MyMain {
     // it is returned unchanged. 
     public static char toLower(char ch) {
         //this is a capital letter
-        if (ch >= 65 && ch <=90) return (char) ((char) ch + 32);
-        return (char) (ch+32);
-
-
-    }
-
-    // Changes a lowercase letter to an uppercase letter
-    // If the char is not a lowercase letter,
-    // it is returned unchanged.
-    public static char toUpper(char ch) {
-        if (ch >= 65 && ch <=90){
-            return ch;
-
-        }
-
-        return (char) (ch - 32);
-    }
-
-    // The method is given a String as input and returns a
-    // new String where all of the numbers have been removed
-    // from the original String.
-    public static String removeNumbers(String str) {
-       //between 48 and 57
-        String total = "";
-        for (int i = 0; i<= str.length();i++){
-            char ch = (char) i;
-            if (ch <48 || ch >57){
-                total += str.charAt(i);
+        if ((ch >= 'a' && ch<= 'z') || (ch>='A' && ch<= 'Z')) {
+            if (ch >= 'a' && ch <= 'z') {
+                return ch;
             }
-            return total;
-
+            return (char) (ch + 32);
         }
+        return ch;
+    }
 
 
+            // Changes a lowercase letter to an uppercase letter
+            // If the char is not a lowercase letter,
+            // it is returned unchanged.
+            public static char toUpper ( char ch){
+                if ((ch >= 'a' && ch<= 'z') || (ch>='A' && ch<= 'Z')) {
+                    if (ch >= 'a' && ch <= 'z') {
+                        return (char) (ch - 32);
+                    }
+                    return ch;
+                }
+                return ch;
+            }
 
-    // Write some code to test your methods!
-    public static void main(String[] args) {
-        // ADD SOME TESTING CODE HERE
-        {
-}
+            // The method is given a String as input and returns a
+            // new String where all of the numbers have been removed
+            // from the original String.
+            public static String removeNumbers (String str){
+                //between 48 and 57
+                String total = "";
+                for (int i = 0; i < str.length(); i++) {
+                    char ch = str.charAt(i);
+                    if (ch < 48 || ch > 57) {
+                        total += ch;
+                    }
+
+                }
+                return total;
+            }
+
+
+            // Write some code to test your methods!
+            public static void main (String[]args){
+                // ADD SOME TESTING CODE HERE
+            }
+        }
